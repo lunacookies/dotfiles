4d7e888c9bd8544e55e01e3745551226abfdda5f
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-04-02 17:10:32 +1100

    [install] Refactor to allow linking init.zsh

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-xdg_dir="$HOME/.config"
-
-rm_if_exists "$xdg_dir"
-ln -s "$PWD/src"  "$xdg_dir"
-
 function rm_if_exists() {
     if [ -e "$1" ]; then
         rm -r "$1"
     fi
 }
+
+function link_config() {
+    rm_if_exists "$2"
+
+    ln -s "$PWD/$1" "$2"
+}
+
+link_config src "$HOME/.config"
+link_config src/zsh/init.zsh "$HOME/.zshrc"
