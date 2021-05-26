f6ffdd0e622f97f234e629adcf03893bdd59a77e
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-05-26 13:55:07 +1000

    [shell] Don’t warn for missing debug implementations

--- src/fish/config.fish
+++ src/fish/config.fish
@@ -20,9 +20,7 @@ if test ! -e "$CARGO_TARGET_DIR"
     mkdir -p "$CARGO_TARGET_DIR"
 end
 
-set -gx RUSTFLAGS "--codegen target-cpu=native \
-    --warn unreachable-pub \
-    --warn missing-debug-implementations"
+set -gx RUSTFLAGS "--codegen target-cpu=native --warn unreachable-pub"
 
 # Fix issue with ncurses GPG pinentry not connecting to TTY properly.
 set -gx GPG_TTY (tty)
