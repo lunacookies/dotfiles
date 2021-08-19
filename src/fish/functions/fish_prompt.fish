9415ce0cbf35aa40d08f24a1f1e4fdcb97539955
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-08-19 20:56:20 +1000

    [shell] Adjust colours in prompt

--- src/fish/functions/fish_prompt.fish
+++ src/fish/functions/fish_prompt.fish
@@ -9,24 +9,24 @@ function fish_prompt
 
     if test -z $SSH_TTY
     else
-        set_color brblue
+        set_color brgreen
         printf "%s" (whoami)
         set_color normal
         printf "@"
-        set_color blue
+        set_color green
         printf "%s " (hostname)
     end
 
-    set_color brgreen
+    set_color brmagenta
     printf $pwd
 
     if set branch (git rev-parse --abbrev-ref HEAD 2> /dev/null)
-	    set_color green
+	    set_color magenta
         printf " $branch"
     end
 
-    set_color normal
-    printf " \$ "
+    set_color magenta
+    printf " → "
 
     printf "\e[6 q" # use line-shaped cursor
 end
