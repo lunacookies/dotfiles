ecc09d9938dc3dd5765fe05b879287c9cd7dbebb
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-08-20 22:27:03 +1000

    [shell] Match syntax highlighting to Gamut

--- src/fish/set_universals.fish
+++ src/fish/set_universals.fish
@@ -1,15 +1,16 @@
 set -U fish_greeting ""
 
-set -U fish_color_command blue
-set -U fish_color_keyword magenta
+set -U fish_color_command magenta
+set -U fish_color_keyword --bold cyan
 set -U fish_color_operator normal
 set -U fish_color_redirection normal
-set -U fish_color_param normal
+set -U fish_color_param green
 set -U fish_color_end normal
-set -U fish_color_quote green
+set -U fish_color_quote red
 set -U fish_color_escape normal
 set -U fish_color_comment brblack
-set -U fish_color_error red --underline
+set -U fish_color_error --underline red
+set -U fish_color_autosuggestion brblack
 
 abbr --add c cargo
 abbr --add g git
