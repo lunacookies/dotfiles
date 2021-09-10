2bd208da72028bd85c77527d1f615c851c238706
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-09-10 18:06:07 +1000

    [shell] Match syntax highlighting to Pale Fire

--- src/fish/set_universals.fish
+++ src/fish/set_universals.fish
@@ -1,15 +1,15 @@
 set -U fish_greeting ""
 
-set -U fish_color_command magenta
-set -U fish_color_keyword --bold cyan
+set -U fish_color_command '#9CDBDE'
+set -U fish_color_keyword --bold '#EADFAF'
 set -U fish_color_operator normal
 set -U fish_color_redirection normal
-set -U fish_color_param green
+set -U fish_color_param normal
 set -U fish_color_end normal
-set -U fish_color_quote red
-set -U fish_color_escape normal
-set -U fish_color_comment brblack
-set -U fish_color_error --underline red
+set -U fish_color_quote '#D49E9E'
+set -U fish_color_escape '#9AC3E4'
+set -U fish_color_comment '#779A77'
+set -U fish_color_error --underline '#D49E9E'
 set -U fish_color_autosuggestion brblack
 
 abbr --add c cargo
