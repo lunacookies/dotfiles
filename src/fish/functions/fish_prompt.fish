06843adf4132d8adfc5007775c9f90d09d740eec
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-11-24 19:50:55 +1100

    [shell] Merge left and right prompts into multi-line prompt

--- src/fish/functions/fish_prompt.fish
+++ src/fish/functions/fish_prompt.fish
@@ -3,11 +3,24 @@ function fish_prompt
     # as the title of the current tmux window
     tmux rename-window (basename (prompt_pwd))
 
+    printf "\n"
+
     if test -z $SSH_TTY
     else
         set_color --bold red
         printf "%s@%s " (whoami) (hostname)
+        set_color normal
     end
+
+    set_color brblack
+    printf "%s " (prompt_pwd)
+
+    if set branch (git rev-parse --abbrev-ref HEAD 2> /dev/null)
+        set_color green
+        printf "$branch "
+    end
+
+    printf "\n"
 
     set_color normal
     printf "λ "
