13b7339d93467d0cecb7dbbc4e16dadf2c7a80de
Aramis Razzaghipour <aramisnoah@gmail.com>
2022-01-03 21:54:53 +1100

    [shell] Match syntax highlighting with Darcula

--- src/fish/set_universals.fish
+++ src/fish/set_universals.fish
@@ -1,15 +1,15 @@
 set -U fish_greeting ""
 
-set -U fish_color_command 9CDBDE
-set -U fish_color_keyword --bold EADFAF
+set -U fish_color_command CC7832
+set -U fish_color_keyword CC7832
 set -U fish_color_operator white
 set -U fish_color_redirection white
 set -U fish_color_param white
 set -U fish_color_end white
-set -U fish_color_quote D49E9E
-set -U fish_color_escape 9AC3E4
-set -U fish_color_comment 779A77
-set -U fish_color_error --underline F5BEBD
+set -U fish_color_quote 6A8759
+set -U fish_color_escape CC7832
+set -U fish_color_comment 79828B
+set -U fish_color_error BC3F3C
 set -U fish_color_autosuggestion brblack
 
 abbr --add c cargo
