fa7edb6803084b3def15c933386447d8ac043fe2
Aramis Razzaghipour <aramisnoah@gmail.com>
2022-01-18 16:20:51 +1100

    Use VS Code Default Dark+

--- src/fish/set_universals.fish
+++ src/fish/set_universals.fish
@@ -1,15 +1,15 @@
 set -U fish_greeting ""
 
-set -U fish_color_command CC7832
-set -U fish_color_keyword CC7832
+set -U fish_color_command DCDCAA
+set -U fish_color_keyword 569CD6
 set -U fish_color_operator white
 set -U fish_color_redirection white
 set -U fish_color_param white
 set -U fish_color_end white
-set -U fish_color_quote 6A8759
-set -U fish_color_escape CC7832
-set -U fish_color_comment 79828B
-set -U fish_color_error BC3F3C
+set -U fish_color_quote CE9178
+set -U fish_color_escape D7BA7D
+set -U fish_color_comment 6A9955
+set -U fish_color_error F44747
 set -U fish_color_autosuggestion brblack
 
 abbr --add c cargo
