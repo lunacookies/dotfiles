0277f7b22bb49758d83648ac02dea3b22778a949
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-02-05 22:34:14 +1100

    [install] Reformat

--- install.sh
+++ install.sh
@@ -1,44 +1,44 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-function rm_if_exists() {
-    if [ -e "$1" ]; then
-        rm -r "$1"
-    fi
+rm_if_exists() {
+	if [ -e "$1" ]; then
+		rm -r "$1"
+	fi
 }
 
-function link_config() {
-    rm_if_exists "$2"
+link_config() {
+	rm_if_exists "$2"
 
-    parent_dir=$(dirname "$2")
-    mkdir -p "$parent_dir"
+	parent_dir=$(dirname "$2")
+	mkdir -p "$parent_dir"
 
-    ln -sf "$PWD/$1" "$2"
+	ln -sf "$PWD/$1" "$2"
 }
 
-function hide_login_message() {
-    touch "$HOME/.hushlogin"
+hide_login_message() {
+	touch "$HOME/.hushlogin"
 }
 
-function brew_install() {
-    brew bundle --verbose
+brew_install() {
+	brew bundle --verbose
 }
 
-function brew_cleanup() {
-    brew bundle cleanup
+brew_cleanup() {
+	brew bundle cleanup
 
-    while true; do
-        printf "\n\nDo you wish to continue? [y/n] "
-        read yn
-        case $yn in
-            [Yy]* ) brew bundle cleanup --force; break;;
-            [Nn]* ) break;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
+	while true; do
+		printf "\n\nDo you wish to continue? [y/n] "
+		read yn
+		case $yn in
+			[Yy]* ) brew bundle cleanup --force; break;;
+			[Nn]* ) break;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
 }
 
-function cleanup_brewfile_lock() {
-    rm "Brewfile.lock.json"
+cleanup_brewfile_lock() {
+	rm "Brewfile.lock.json"
 }
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
