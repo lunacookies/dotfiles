b8293cd4d3d3ab93ab757a06848c176175ebefd6
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-02-19 11:41:42 +1100

    [shell] Make git status in prompt a different color to branch

--- src/fish/functions/fish_prompt.fish
+++ src/fish/functions/fish_prompt.fish
@@ -4,7 +4,8 @@ function fish_prompt
 	set -l bracket_color brblack
 	set -l status_color brred
 	set -l pwd_color brcyan
-	set -l git_color brgreen
+	set -l git_branch_color brgreen
+	set -l git_status_color bryellow
 
 	printf "%s[%s" (set_color $bracket_color) (set_color normal)
 
@@ -15,25 +16,28 @@ function fish_prompt
 	printf "%s%s%s" (set_color $pwd_color) (prompt_pwd) (set_color normal)
 
 	if git rev-parse --is-inside-work-tree &> /dev/null
-		set -l git_info ( \
+		set -l git_branch ( \
 			git symbolic-ref --quiet --short HEAD || \
 			git describe --tags --exact-match HEAD || \
 			git rev-parse --short HEAD
 		)
+		printf  " %s%s%s" (set_color $git_branch_color) $git_branch (set_color normal)
+
+		set -l git_status
 
 		# dirty
 		if not git diff-files --no-ext-diff --quiet
-			set git_info "$git_info*"
+			set git_status "$git_status*"
 		end
 
 		# staged
 		if not git diff-index --cached --no-ext-diff --quiet HEAD 2> /dev/null
-			set git_info "$git_info+"
+			set git_status "$git_status+"
 		end
 
 		# stash
 		if git rev-parse --quiet --verify refs/stash > /dev/null
-			set git_info "$git_info^"
+			set git_status "$git_status^"
 		end
 
 		# untracked
@@ -49,20 +53,20 @@ function fish_prompt
 
 		# only print if we had no untracked files output
 		if test -n "$untracked_files"
-			set git_info "$git_info?"
+			set git_status "$git_status?"
 		end
 
 		set -l unpushed_commits (git rev-list '@{u}..HEAD')
 		if test -n "$unpushed_commits"
-			set git_info "$git_info↑"
+			set git_status "$git_status↑"
 		end
 
 		set -l unpulled_commits (git rev-list 'HEAD..@{u}')
 		if test -n "$unpulled_commits"
-			set git_info "$git_info↓"
+			set git_status "$git_status↓"
 		end
 
-		printf  " %s%s%s" (set_color $git_color) $git_info (set_color normal)
+		printf  "%s%s%s" (set_color $git_status_color) $git_status (set_color normal)
 	end
 
 	printf "%s]%s " (set_color $bracket_color) (set_color normal)
