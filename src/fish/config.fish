4b45add0cc7efa20c8647809e121d7b1c50522a3
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-02-19 11:35:32 +1100

    [shell] Move universals into config.fish

--- src/fish/config.fish
+++ src/fish/config.fish
@@ -15,3 +15,33 @@ set -gx MANPAGER "nvim +Man!"
 
 # wrap manpage to a narrower width for ease of reading
 set -gx MANWIDTH 72
+
+rm ~/.config/fish/fish_variables
+set -g fish_greeting ""
+set -g fish_color_command brblue
+set -g fish_color_keyword brblue
+set -g fish_color_operator brblue
+set -g fish_color_redirection white
+set -g fish_color_param white
+set -g fish_color_end brblue
+set -g fish_color_quote brgreen
+set -g fish_color_escape green
+set -g fish_color_comment brblack
+set -g fish_color_error --underline red
+set -g fish_color_autosuggestion brblack
+
+abbr --add c cargo
+abbr --add cargo echo "use c!"
+
+abbr --add g git
+abbr --add git echo "use g!"
+
+abbr --add vi nvim
+abbr --add nvim echo "use vi!"
+abbr --add vim echo "use vi!"
+
+abbr --add b brew
+abbr --add brew echo "use b!"
+
+abbr --add m make -j10
+abbr --add make echo "use m!"
